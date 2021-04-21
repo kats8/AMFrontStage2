c24380bb57bcdb1ed3d966f538c8ebd8a9fcb699
Katrina Steen <s.katrina@y7mail.com>
2021-04-21 22:09:31 +1000

    VR id matching with DB entries

--- app.js
+++ app.js
@@ -3,6 +3,15 @@
 const MongoClient = require('mongodb').MongoClient;
 var express = require("express");
 var bodyParser = require("body-parser");
+const uri = "mongodb+srv://Angler_User:89CL735AU@sit725.63pic.mongodb.net/AM_Fish?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+//variable used for MongoDB collection of fish and regulations
+let fishes;
+client.connect(err => {
+  fishes = client.db("AM_Fish").collection("FishRegs");
+  console.log("Connected to database");
+  //    client.close();
+});
 
 const VisualRecognitionV3 = require('ibm-watson/visual-recognition/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
@@ -39,12 +48,13 @@ function analyseImage(requestURL, response) {
     url: requestURL,
     threshold: 0.6,
   };
-  //response.json("Hello " + user_name + "!");
+
   visualRecognition.classify(vrParams)
     .then(VRresponse => {
       var imageResults = VRresponse.result;
-      console.log(JSON.stringify(imageResults.images[0].classifiers[0].classes[0].class, null, 1));
-      response.send(JSON.stringify({ data: imageResults.images[0].classifiers[0].classes[0].class }))
+      console.log(JSON.stringify(imageResults.images[0].classifiers[0].classes, null, 1));
+      // response.send(JSON.stringify({ body: imageResults.images[0].classifiers[0].classes[0].class }))
+      response.send(JSON.stringify({ body: imageResults.images[0].classifiers[0].classes }));
     })
     .catch(err => {
       console.log('error:', err);
@@ -56,17 +66,7 @@ app.get("/classifyURL", function (request, response) {
   analyseImage(request.query.url, response);
 });
 
-/*
-visualRecognition.classify(classifyParams)
-  .then(response => {
-    const classifiedImages = response.result;
-    console.log(JSON.stringify(classifiedImages, null, 2));
-  })
-  .catch(err => {
-    console.log('error:', err);
-  });
 
-  */
 //DATABASE MANAGEMENT
 
 
@@ -78,6 +78,12 @@ visualRecognition.classify(classifyParams)
 app.get("/getFishInfo", function (request, response) {
   let fishID = request.query.fish;
   returnInfo(fishID, response);
+});
+
+app.get("/checkFishMatch", function (request, response) {
+  let objectsFound = request.query.body;
+  console.log("this is objects Found in checkFishMatch " + objectsFound);
+  checkForFish(objectsFound, response);
 });
 
 /*
@@ -109,7 +115,7 @@ async function run() {
 
 run().catch(console.dir);
 }*/
-
+/*
 const uri = "mongodb+srv://Angler_User:89CL735AU@sit725.63pic.mongodb.net/AM_Fish?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 let fishes;
@@ -118,8 +124,8 @@ client.connect(err => {
   console.log("Connected to database");
   //    client.close();
 });
-
-
+*/
+//function to check a single image 'class' name to see if it matches a fish
 function returnInfo(fishName, response) {
   let fishData = null;
   // fishes.createIndex({fish:-1}, { collation: { locale: 'en', strength: 2 } } );
@@ -132,28 +138,67 @@ function returnInfo(fishName, response) {
         protected: fishes.protected,
         info: fishes.info
       }
-     
+
       console.log(fishData);
       response.send(JSON.stringify(fishData));
 
     }
-  //  if (err) throw err;
-
+    //  if (err) throw err;
   })
 
-  /*
-  fishes.find().sort({ Fish: 1 }).limit(10).toArray(function (err, result) {
-    if (err) throw err;
-    console.log(result);
-    response.send(JSON.stringify(result));
-  });
-  */
-  //db.fishes.find( { fish: fishName }, { fish: 1, info: 1, noxious: 1, protected: 1, _id: 0 } } )
-  // perform actions on the collection object
+}
+//function takes array of classes (identified class/item names) and scores, and checks for match with fish DB
+function checkForFish(idfdObjectArray, response) {
+  //tracks if a match was found
+  let fishMatch = false;
+  //tracks score of best match so far
+  let score = 0;
+  let fishData = { fishMatch: fishMatch };
+  //let objectArray = JSON.parse(idfdObjectArray);
+  let objectArray = idfdObjectArray;
+  console.log(idfdObjectArray);
+
+  try {
+    console.log("Checking database");
+    fishes = client.db("AM_Fish").collection("FishRegs");
+    //checkForFish(classesFound);
+    objectArray.forEach((idfdObject) => {
+      //check the fishRegulations database for a match of fish type 
+      fishes.find().forEach(function (fishes) {
+        //***** */
+        console.log("I'm in here");
+        //if a match is found, check if it scores better than current match score, if so, replace data (better match).
+        if (fishes.fish.toLowerCase() == (idfdObject.class).toLowerCase()) {
+          fishMatch = true;
+          if (idfdObject.score > score) {
+            fishData = {
+              fish: fishes.fish,
+              noxious: fishes.noxious,
+              protected: fishes.protected,
+              info: fishes.info,
+              score: score,
+              fishMatch: fishMatch
+            }
+            console.log("found a matcg");
+            console.log(fishData);
+
+          }
+
+        }
+      })
+    })
+    console.log(fishData);
+    // response.send(JSON.stringify(fishData));
+    response.send(JSON.stringify(fishData));
+
+
+  } catch (e) {
+    console.error(e);
+
+  }
 
 
 }
-
 //testing - previously worked
 function returnInfo1(fishName, response) {
   // fishes.createIndex({fish:-1}, { collation: { locale: 'en', strength: 2 } } );
--- public/env.js
+++ public/env.js
@@ -1,19 +1,30 @@
-$(document).ready(function(){
-    console.log('Ready');
+$(document).ready(function () {
+  console.log('Ready');
 
-    $('#btnClassify').click(()=>{
-      
-      let inputURL=$('#urlBox').val();
-      let input = {
-        url: inputURL
-      }
-      $.get('/classifyURL', input, function(result){
-        var displayResult = jQuery.parseJSON(result);
-        $('#textInfo').html(displayResult.data);
-        $('#urlPic').attr("src", inputURL);
-        //alert(result.images[0].classifiers[0].classes[0].class);        
+  $('#btnClassify').click(() => {
+
+    let inputURL = $('#urlBox').val();
+    let input = {
+      url: inputURL
+    }
+    $.get('/classifyURL', input, function (result) {
+      var imageResult = jQuery.parseJSON(result);
+     // $('#textInfo').html(imageResult.body[0].class);
+      $('#urlPic').attr("src", inputURL);
+      //trying the remote way
+      //DO THIS IN A MINUTE
+      $.get("/checkFishMatch", imageResult, function (matchData) {
+        if (matchData.fishMatch) {
+        $('#textInfo').html(matchData.info)
+        }
+        else {
+          $('#textInfo').html("Looks like a "+imageResult.body[0].class+ ", but we couldn't match it with a fish species on our database. More species are coming soon!");
+
+        }
       })
+
+      //alert(result.images[0].classifiers[0].classes[0].class);        
     })
-
-
   })
+
+})
