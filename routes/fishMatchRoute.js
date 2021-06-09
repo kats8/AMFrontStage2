b5a2c5e31e8f03a97e840f1acc25340d9df7e8fe
Katrina Steen <s.katrina@y7mail.com>
2021-06-10 05:06:15 +1000

    back to cloud after testing

--- routes/fishMatchRoute.js
+++ routes/fishMatchRoute.js
@@ -20,10 +20,10 @@ router.get('/', function (request, response) {
     }
     
     //link for local testing (http://localhost:8081/) ------
-    reqObject = "http://localhost:8081/checkFishMatch?body=" + JSON.stringify(inBody)+ "&lat="+pLat+"&long="+pLong+"&url="+imageURL;
+    //reqObject = "http://localhost:8081/checkFishMatch?body=" + JSON.stringify(inBody)+ "&lat="+pLat+"&long="+pLong+"&url="+imageURL;
     //----------
     //access via cloud (PAAS)
-    //reqObject = "https://anglermatehub.us-south.cf.appdomain.cloud/checkFishMatch?body=" + JSON.stringify(inBody)+ "&lat="+pLat+"&long="+pLong+"&url="+imageURL;
+    reqObject = "https://anglermatehub.us-south.cf.appdomain.cloud/checkFishMatch?body=" + JSON.stringify(inBody)+ "&lat="+pLat+"&long="+pLong+"&url="+imageURL;
     req(reqObject, (err, result) => {
   
       //if true match being returned in response, initiate alert via webserver
